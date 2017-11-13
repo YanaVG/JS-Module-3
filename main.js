d8e9f3332445b4f818334f910ee9ef887ea53140
YanaVG <yanavg18@gmail.com>
2017-11-13 13:41:26 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+console.log(alphabet.indexOf('h'));
+console.log(alphabet.indexOf('e'));
+console.log(alphabet.indexOf('l'));
+console.log(alphabet.indexOf('o'));
+
+let words1 = alphabet[17]  + alphabet[2] + alphabet[20] + alphabet[20] + alphabet[8];
+console.log(words1);
+
+console.log(alphabet.indexOf('j'));
+console.log(alphabet.indexOf('a'));
+console.log(alphabet.indexOf('v'));
+console.log(alphabet.indexOf('a'));
+console.log(alphabet.indexOf('s'));
+console.log(alphabet.indexOf('c'));
+console.log(alphabet.indexOf('r'));
+console.log(alphabet.indexOf('i'));
+console.log(alphabet.indexOf('p'));
+console.log(alphabet.indexOf('t'));
+let words2 = alphabet[18]  + alphabet[12] + alphabet[26] + alphabet[12] + alphabet[13] + alphabet[25]  + alphabet[3] + alphabet[7] + alphabet[9] + alphabet[4];
+console.log(words2);
+
+console.log(alphabet.indexOf('t'));
+console.log(alphabet.indexOf('r'));
+console.log(alphabet.indexOf('a'));
+console.log(alphabet.indexOf('i'));
+console.log(alphabet.indexOf('n'));
+console.log(alphabet.indexOf('e'));
+console.log(alphabet.indexOf('r'));
+
+let words3;
+console.log(alphabet[4] + alphabet[3] + alphabet[12] + alphabet[7] + alphabet[28] + alphabet[2]  + alphabet[3]);
